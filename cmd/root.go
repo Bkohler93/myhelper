60b496008f8960910a4ea0a1e863cc7650959292
Brett Kohler <brettkohler93@gmail.com>
2026-04-07 12:50:36 -0500

    feat(1-1): scaffold cobra CLI with root command
    - Add cobra dependency to go.mod/go.sum
    - Create main.go entrypoint calling cmd.Execute()
    - Create cmd/root.go with rootCmd definition

---
+++ cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "myhelper",
+	Short: "A focused Go coding assistant backed by a local Ollama model",
+	Long:  "myhelper sends focused prompts to a local Ollama server and streams responses to stdout.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/bkohler93/my-helper/cmd"
+
+func main() {
+	cmd.Execute()
+}
